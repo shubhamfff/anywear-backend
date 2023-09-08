5b5da9070a4b64887deef4bb10027404431b0121
shubhamfff <shubham.shetyanavar@gmail.com>
2023-09-08 13:33:05 +0530

    mongooes connected

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require("express");
+import express from "express";
+import connectDB from "./config/db.js";
+import cors from "cors";
 
 const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 8080;
 
