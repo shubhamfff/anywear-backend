8d50e44d0a89140e72af6d7becf79d1a749e5c9e
shubhamfff <shubham.shetyanavar@gmail.com>
2023-09-08 13:47:56 +0530

    backend added

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 import express from "express";
 import connectDB from "./config/db.js";
 import cors from "cors";
-
-const app = express();
+import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import morgan from "morgan";
 
 connectDB();
 
-const PORT = process.env.PORT || 8080;
+const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`connected to ${PORT}`);
